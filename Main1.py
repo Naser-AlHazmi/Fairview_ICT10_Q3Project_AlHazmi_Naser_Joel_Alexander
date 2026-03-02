aa9bf55e4e6626f8f8a417387b05ba1a25679adc
Naser-AlHazmi <alhazmi.na@obmontessori.edu.ph>
2026-03-03 07:42:23 +0800

    Refactor Signup function to include username validation

--- Main1.py
+++ Main1.py
@@ -6,11 +6,13 @@ def Signup(e):
 
     name = document.getElementById("name").value #input value
 
+    username = document.getElementById("username").value #input value
+
     password = document.getElementById("password").value #input value
 
     # check if name and password are not empty
 
-    if not name or not password: #if either is empty
+    if not username or not password: #if either is empty
 
         document.getElementById("output").innerHTML = "Please enter both username and password." #display error message
 
@@ -28,31 +30,31 @@ def Signup(e):
 
         e.preventDefault()
 
-    elif name == "Dela Cruz" and password == "Dela Cruz123":
-
-        document.location.href = "team.html"
-
-        e.preventDefault()
-
-    elif name == "Juan Miguel" and password == "Juan Miguel123":
-
-        document.location.href = "team.html"
-
-        e.preventDefault()
-
     elif name == "cortisolspike" and password == "cortisolspike123": #Secret
 
         webbrowser.open("https://youtube.com/shorts/4djt69C-Dfw?si=UC_luv1dXEEh1WvZ")
 
         e.preventDefault()
 
-    elif len(password) >= 10 and len(name) >= 7: #if password and name is valid
+    elif name == "Dela Cruz" and len(username) >= 7 and password == "Dela Cruz123": #if name is "Dela Cruz" and username is valid and password is valid
+
+        document.location.href = "team.html" 
+
+        e.preventDefault()
+
+    elif name == "Juan Miguel" and len(username) >= 7 and password == "Juan Miguel123": #if name is "Juan Miguel" and username is valid and password is valid
+
+        document.location.href = "team.html"
+
+        e.preventDefault()
+
+    elif len(password) >= 10 and len(username) >= 7: #if password and username is valid
 
         if not password.isalpha(): #if password contains only letters
     
             if not password.isdigit(): #if password contains only digits
 
-                document.location.href = "team.html" #moves to team.html
+                document.location.href = "team.html" 
 
                 e.preventDefault()
 
@@ -67,4 +69,3 @@ def Signup(e):
         document.getElementById("output").innerHTML = "Invalid username/password! Username must contain 7 letters, and Password must be at least 10 characters long and contain both letters and numbers." #display error message
 
         e.preventDefault()
-
